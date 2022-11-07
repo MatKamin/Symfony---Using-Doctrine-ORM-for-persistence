a4f1036d78c02215c295caa2e69978f774e2a951
Viktor Karpinski <viktorkarpinski@Viktors-MacBook-Pro-2.local>
2022-11-07 11:07:27 +0100

    Migration fix

---
+++ migrations/Version20221107100652.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221107100652 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE movie (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, release_year INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE quote (id INT AUTO_INCREMENT NOT NULL, movie_id INT NOT NULL, content LONGTEXT NOT NULL, author VARCHAR(255) NOT NULL, INDEX IDX_6B71CBF48F93B6FC (movie_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE quote ADD CONSTRAINT FK_6B71CBF48F93B6FC FOREIGN KEY (movie_id) REFERENCES movie (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE quote DROP FOREIGN KEY FK_6B71CBF48F93B6FC');
+        $this->addSql('DROP TABLE movie');
+        $this->addSql('DROP TABLE quote');
+    }
+}
